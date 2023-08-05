ebaf85eda9e56e1f59e5bc6aee554464f481c2ca
Farruh1420 <farik_17_88@mail>
2023-08-05 15:58:25 +0500

    1

--- lesson3/main.py
+++ lesson3/main.py
@@ -1,4 +1,4 @@
-tiket = int(input('введите ваш номер'))
-result = tiket // 4 + 1
+# tiket = int(input('введите ваш номер'))
+# result = tiket // 4 + 1
 # print(result)
-print(f'ваш купе под номером: {result}')
+# print(f'ваш купе под номером: {result}')
